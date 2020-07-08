5a278aa36fb88919047c0110a3937eece84b1ec5
Thorsten Neff <Thorsten.Neff@tk.de>
2020-07-08 09:35:14 +0200

    Kotlinize custom scopes

--- app/src/main/java/com/playground/daggerscopes/di/ActivityScope.kt
+++ app/src/main/java/com/playground/daggerscopes/di/ActivityScope.kt
@@ -3,6 +3,5 @@ package com.playground.daggerscopes.di
 import javax.inject.Scope
 
 @Scope
-@MustBeDocumented
-@Retention(AnnotationRetention.RUNTIME)
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
 annotation class ActivityScope
